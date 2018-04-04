1b06768d31fbe21cd553c7b8103a66a1f751a5d9
mambax7 <mambax7@gmail.com>
2018-04-04 08:14:31 -0400

    updates
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onuninstall.php
+++ include/onuninstall.php
@@ -8,6 +8,7 @@
  * @link            https://xoops.org XOOPS
  */
 
+use XoopsModules\Xlanguage;
 
 /**
  * Prepares system prior to attempting to uninstall module
@@ -16,7 +17,7 @@
  * @return bool true if ready to uninstall, false if not
  */
 
-function xoops_module_pre_uninstall_xxxx(\XoopsModule $module)
+function xoops_module_pre_uninstall_xlanguage(\XoopsModule $module)
 {
     // Do some synchronization
     return true;
@@ -29,21 +30,20 @@ function xoops_module_pre_uninstall_xxxx(\XoopsModule $module)
  *
  * @return bool true if uninstallation successful, false if not
  */
-function xoops_module_uninstall_xxxx(\XoopsModule $module)
+function xoops_module_uninstall_xlanguage(\XoopsModule $module)
 {
 //    return true;
 
     $moduleDirName = basename(dirname(__DIR__));
-    $xsitemapHelper      = \Xmf\Module\Helper::getHelper($moduleDirName);
+    $moduleDirNameUpper = strtoupper($moduleDirName);
+     $helper      =Xlanguage\Helper::getInstance();
 
-    /** @var XXXXXXUtility $utilityClass */
-    $utilityClass     = ucfirst($moduleDirName) . 'Utility';
-    if (!class_exists($utilityClass)) {
-        xoops_load('utility', $moduleDirName);
-    }
+    /** @var Xlanguage\Utility $utility */
+    $utility     = new Xlanguage\Utility();
+
 
     $success = true;
-    $xsitemapHelper->loadLanguage('admin');
+    $helper->loadLanguage('admin');
 
 
     //------------------------------------------------------------------
@@ -55,7 +55,7 @@ function xoops_module_uninstall_xxxx(\XoopsModule $module)
         $dirInfo = new \SplFileInfo($old_dir);
         if ($dirInfo->isDir()) {
             // The directory exists so delete it
-            if (false === $utilityClass::rrmdir($old_dir)) {
+            if (false === $utility::rrmdir($old_dir)) {
                 $module->setErrors(sprintf(constant('CO_' . $moduleDirNameUpper . '_ERROR_BAD_DEL_PATH'), $old_dir));
                 $success = false;
             }
--- include/onupdate.php
+++ include/onupdate.php
@@ -73,25 +73,12 @@ function xoops_module_update_xlanguage(\XoopsModule $module, $previousVersion = 
 
     /** @var Xlanguage\Helper $helper */
     /** @var Xlanguage\Utility $utility */
-    /** @var Xlanguage\Configurator $configurator */
+    /** @var Xlanguage\Common\Configurator $configurator */
     $helper  = Xlanguage\Helper::getInstance();
     $utility = new Xlanguage\Utility();
-    $configurator = new Xlanguage\Configurator();
+    $configurator = new Xlanguage\Common\Configurator();
 
     if ($previousVersion < 240) {
-
-        //rename column EXAMPLE
-        $tables     = new Tables();
-        $table      = 'xlanguagex_categories';
-        $column     = 'ordre';
-        $newName    = 'order';
-        $attributes = "INT(5) NOT NULL DEFAULT '0'";
-        if ($tables->useTable($table)) {
-            $tables->alterColumn($table, $column, $attributes, $newName);
-            if (!$tables->executeQueue()) {
-                echo '<br>' . _AM_XXXXX_UPGRADEFAILED0 . ' ' . $migrate->getLastError();
-            }
-        }
 
         //delete old HTML templates
         if (count($configurator->templateFolders) > 0) {
@@ -138,7 +125,7 @@ function xoops_module_update_xlanguage(\XoopsModule $module, $previousVersion = 
         if (count($configurator->uploadFolders) > 0) {
             //    foreach (array_keys($GLOBALS['uploadFolders']) as $i) {
             foreach (array_keys($configurator->uploadFolders) as $i) {
-                $utilityClass::createFolder($configurator->uploadFolders[$i]);
+                $utility::createFolder($configurator->uploadFolders[$i]);
             }
         }
 
@@ -147,7 +134,7 @@ function xoops_module_update_xlanguage(\XoopsModule $module, $previousVersion = 
             $file = __DIR__ . '/../assets/images/blank.png';
             foreach (array_keys($configurator->copyBlankFiles) as $i) {
                 $dest = $configurator->copyBlankFiles[$i] . '/blank.png';
-                $utilityClass::copyFile($file, $dest);
+                $utility::copyFile($file, $dest);
             }
         }
 
@@ -155,9 +142,9 @@ function xoops_module_update_xlanguage(\XoopsModule $module, $previousVersion = 
         $sql = 'DELETE FROM ' . $GLOBALS['xoopsDB']->prefix('tplfile') . " WHERE `tpl_module` = '" . $module->getVar('dirname', 'n') . '\' AND `tpl_file` LIKE \'%.html%\'';
         $GLOBALS['xoopsDB']->queryF($sql);
 
-        /** @var XoopsGroupPermHandler $gpermHandler */
-        $gpermHandler = xoops_getHandler('groupperm');
-        return $gpermHandler->deleteByModule($module->getVar('mid'), 'item_read');
+        /** @var XoopsGroupPermHandler $grouppermHandler */
+        $grouppermHandler = xoops_getHandler('groupperm');
+        return $grouppermHandler->deleteByModule($module->getVar('mid'), 'item_read');
     }
     return true;
 }
