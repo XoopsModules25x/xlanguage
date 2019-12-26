215a1082f2f0ba947e23871bec1ab8eb9f959d8c
mambax7 <mambax7@gmail.com>
2019-12-26 03:11:53 -0500

    decreased index key length

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -11,7 +11,7 @@ CREATE TABLE `xlanguage_base` (
   `lang_charset` VARCHAR(255)    NOT NULL DEFAULT '',
   `lang_image`   VARCHAR(255)    NOT NULL DEFAULT '',
   PRIMARY KEY (`lang_id`),
-  KEY `lang_name`     (`lang_name`)
+  KEY `lang_name`     (`lang_name`(100))
 )
   ENGINE = MyISAM;
 
@@ -25,7 +25,7 @@ CREATE TABLE `xlanguage_ext` (
   `lang_image`   VARCHAR(255)    NOT NULL DEFAULT '',
   `lang_base`    VARCHAR(255)    NOT NULL DEFAULT '',
   PRIMARY KEY (`lang_id`),
-  KEY `lang_name`     (`lang_name`),
-  KEY `lang_base`     (`lang_base`)
+  KEY `lang_name`     (`lang_name`(100)),
+  KEY `lang_base`     (`lang_base`(100))
 )
   ENGINE = MyISAM;
