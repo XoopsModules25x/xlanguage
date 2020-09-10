37a9e12c1e220084b5f55139cbb4816f2807e14d
mambax7 <mambax7@gmail.com>
2020-09-10 19:14:18 -0400

    Cascading dirname(...) calls

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -22,7 +22,7 @@ use XoopsModules\Xlanguage\Helper;
 
 require dirname(__DIR__) . '/preloads/autoloader.php';
 
-require_once dirname(dirname(dirname(__DIR__))) . '/include/cp_header.php';
+require_once dirname(__DIR__, 3) . '/include/cp_header.php';
 //require_once $GLOBALS['xoops']->path('www/class/xoopsformloader.php');
 
 // require_once  dirname(__DIR__) . '/class/Utility.php';
