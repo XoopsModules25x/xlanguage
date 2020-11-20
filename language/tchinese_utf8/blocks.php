267c5001c3ae0037938c53c7f96ab93324d55cb3
ystartgo <50304042+ystartgo@users.noreply.github.com>
2020-11-21 07:34:18 +0800

    Add tchinese_utf8
    Add tchinese_utf8 Traditional Chinese Language

---
+++ language/tchinese_utf8/blocks.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: blocks.php,v 1.1.1.3 2004/11/08 03:40:00 phppp Exp $
+// Blocks
+define('_MB_XLANGUAGE_SELECT', '點擊以選擇您的語言');
+define('_MB_XLANGUAGE_DISPLAY_METHOD', '顯示方式');
+define('_MB_XLANGUAGE_DISPLAY_FLAGLIST', '圖片清單');
+define('_MB_XLANGUAGE_DISPLAY_TEXTLIST', '文字清單');
+define('_MB_XLANGUAGE_DISPLAY_DROPDOWNLIST', '下拉列表');
+define('_MB_XLANGUAGE_IMAGE_SEPARATOR', '圖像分離器');
+define('_MB_XLANGUAGE_IMAGE_PERROW', '每行圖像');
+define('_MB_XLANGUAGE_OPTIONAL', '可選');
+?>
---
+++ language/tchinese_utf8/modinfo.php
@@ -0,0 +1,42 @@
+<?php
+// $Id: modinfo.php,v 1.2 2004/09/27 20:36:31 phppp Exp $
+// Module Info
+
+define("_MI_XLANGUAGE_NAME","設置多種語言");
+define("_MI_XLANGUAGE_DESC","可擴展的多語言管理");
+
+define("_MI_XLANGUAGE_BNAME","語言選擇");
+
+define("_MI_XLANGUAGE_ADMENU0","語言清單");
+define("_MI_XLANGUAGE_ADMENU1","新增基本語言");
+define("_MI_XLANGUAGE_ADMENU2","新增擴展語言");
+define("_MI_XLANGUAGE_ADMENU3","關於本模組");
+
+define('_MI_XLANGUAGE_AUTHOR_INFO', "開發者資訊");
+define('_MI_XLANGUAGE_AUTHOR_NAME', "開發者");
+define('_MI_XLANGUAGE_AUTHOR_WEBSITE', "開發者網站");
+define('_MI_XLANGUAGE_AUTHOR_EMAIL', "開發者電子郵件");
+define('_MI_XLANGUAGE_AUTHOR_CREDITS', "");
+define('_MI_XLANGUAGE_MODULE_INFO', "模組開發資訊");
+define('_MI_XLANGUAGE_MODULE_STATUS', "開發狀況");
+define('_MI_XLANGUAGE_MODULE_DEMO', "演示網站");
+define('_MI_XLANGUAGE_MODULE_SUPPORT', "官方支持網站");
+define('_MI_XLANGUAGE_RELEASE', "發布日期");
+define('_MI_XLANGUAGE_AUTHOR_BUGFIXES', "錯誤修復歷史");
+define('_MI_XLANGUAGE_MODULE_XOOPSVERSION', "Xoops版本");
+//3.03
+define('_MI_XLANGUAGE_ADMENU_HOME', '首頁');
+//3.05
+//Help
+define('_MI_XLANGUAGE_DIRNAME', basename(dirname(__DIR__, 2)));
+define('_MI_XLANGUAGE_HELP_HEADER', __DIR__ . '/help/helpheader.tpl');
+define('_MI_XLANGUAGE_BACK_2_ADMIN', '返回管理');
+define('_MI_XLANGUAGE_OVERVIEW', '總覽');
+
+//define('_MI_XLANGUAGE_HELP_DIR', __DIR__);
+
+//help multi-page
+define('_MI_XLANGUAGE_DISCLAIMER', '免責聲明');
+define('_MI_XLANGUAGE_LICENSE', '許可證');
+define('_MI_XLANGUAGE_SUPPORT', '支持');
+?>
